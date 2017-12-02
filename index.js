b9f0d5b14015dca1bc95cfef7aab5230d31f330d
codeNgamer <ekeneoduah@live.com>
2017-12-02 17:25:16 -0600

    add ability to serve files and different outputs

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
 'use strict';
 
-const {json} = require('micro');
+const {json, send} = require('micro');
 const jsreport = require('jsreport-core')();
+const cors = require('micro-cors')();
+const _ = require('lodash');
+const FileCleaner = require('cron-file-cleaner').FileCleaner;
+const path = require('path');
+const fs = require('fs');
+const uuidv1 = require('uuid/v1');
+const url = require('url');
+const queryString = require('query-string');
+
+const fileWatcher = new FileCleaner(
+  path.join(__dirname, 'assets'),
+  600000,
+  '* */15 * * * *',
+  {
+    start: true
+  });
+
+const serveFile = async payload => {
+  const { filename, res } = payload;
+  fs.access(
+    path.join(__dirname, 'assets/', `${filename}.pdf`),
+    fs.constants.R_OK | fs.constants.W_OK, (err) => {
+      if (err) return send(res, 404, 'Not Found');
+      const fileStream = fs.createReadStream(
+        path.join(__dirname, 'assets/', `${filename}.pdf`),
+        { flags : 'r' }
+      )
+      fileStream.pipe(res);
+  });
+}
 
 const htmlToPdf = ({ html, options, res }) => new Promise(function(resolve, reject) {
-  // wkhtmltopdf(html, options, function (err, stream) {
-  //   if (err) return reject(err);
-  //   console.log(stream);
-  //   return resolve(stream);
-  // });
+  const outputs = {
+    'url': resp => {
+      const filename = uuidv1();
+      const fileStream = fs.createWriteStream(
+        path.join(__dirname, 'assets/', `${filename}.pdf`),
+        { flags : 'w' }
+      );
+      resp.stream.pipe(fileStream);
+      resolve(filename);
+    },
+    stream: resp => resolve(resp.stream.pipe(res)),
+    buffer: resp => resolve(resp.content.toString('base64')),
+  };
+
   jsreport.init()
     .then(function () {
       return jsreport.render({
@@ -16,24 +55,35 @@ const htmlToPdf = ({ html, options, res }) => new Promise(function(resolve, reje
           content: html,
           engine: 'jsrender',
           recipe: 'wkhtmltopdf',
-          wkhtmltopdf: options,
+          wkhtmltopdf: _.defaults({
+"allowLocalFilesAccess": true
+          }, options)
         },
-        data: {
-          foo: "world"
-        }
+        data: _.defaults({}, options.htmlData)
       }).then(function(resp) {
-        resolve(resp.stream);
+        if (outputs[options.output]) return outputs[options.output](resp);
+        outputs.url(resp);
       });
     }).catch(function(e) {
       reject(e);
     })
 });
 
-module.exports = async (req, res) => {
+const handler = async (req, res) => {
+  if (req.method === 'OPTIONS') {
+    return {}
+  }
+  if (req.method === 'GET') {
+    const { filename } = url.parse(req.url, true).query;
+    if (!filename) return send(res, 404, 'Not Found');
+    return serveFile({ filename: filename, res});
+  }
 	const body = await json(req);
   const {html, options} = body;
 
+
   // htmlToPdf({ html, options, res });
-  const stream = await htmlToPdf({ html, options});
-  stream.pipe(res);
+  return await htmlToPdf({ html, options});
 };
+
+module.exports = cors(handler);
